d94d10870929b711ea754e3c1f7566a3674a44e0
Daniel Felipe Diaz Alzate <diazalzated2304@gmail.com>
2023-05-10 19:04:33 -0500

    first commit

---
+++ src/main/java/co/com/edu/umanizales/ledsleds/LedsledsApplication.java
@@ -0,0 +1,13 @@
+package co.com.edu.umanizales.ledsleds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedsledsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LedsledsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/com/edu/umanizales/ledsleds/LedsledsApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.edu.umanizales.ledsleds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedsledsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
